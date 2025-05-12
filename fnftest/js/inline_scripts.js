3f31a57d6a5545a2da09588a105298571c65362e
First-Person-Forge <leighton6ms@outlook.com>
2025-05-12 20:51:36 +0100

    test

---
+++ fnftest/js/inline_scripts.js
@@ -0,0 +1,15 @@
+
+		window.addEventListener ("touchmove", function (event) { event.preventDefault (); }, { capture: false, passive: false });
+		if (typeof window.devicePixelRatio != 'undefined' && window.devicePixelRatio > 2) {
+			var meta = document.getElementById ("viewport");
+			meta.setAttribute ('content', 'width=device-width, initial-scale=' + (2 / window.devicePixelRatio) + ', user-scalable=no');
+		}
+	
+
+
+		 
+	
+
+
+		lime.embed ("Kade Engine", "openfl-content", 1280, 720, { parameters: {} });
+	
